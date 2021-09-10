34d9ecc0c20461c9e5e038670f12357f569cbfd7
kintari <mphears@gmail.com>
2021-09-09 21:18:47 -0400

    went back to custom scanning routines

--- src/scanner.c
+++ src/scanner.c
@@ -8,26 +8,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-
 #define MAX_STATES 64
 
 #ifndef _countof
 #define _countof(x) (sizeof(x)/sizeof((x)[0]))
 #endif
-/*
-#ifndef strdup
-#define strdup _strdup
-#endif
-*/
+
+#define ALPHA_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define DIGIT_CHARS "0123456789"
+#define SPACE_CHARS " \r\n\t"
+#define MISC_CHARS  "(){}[],.;:+-*/"
+
 struct scanner_t {
 	FILE *file;
 	char *filename;
 	int line, col;
 	int lookahead[2];
 	text_t *text;
-	uint8_t state_tbl[MAX_STATES][256];
-	size_t num_states;
-	int reject_state, ignore_state, accept_state, start_state;
 };
 
 bool eos(scanner_t *scanner) {
@@ -57,18 +54,6 @@ static int read_char(scanner_t *s) {
 	return ret;
 }
 
-static int new_state(scanner_t *scanner) {	
-	int state = (int) scanner->num_states++;
-	for (int i = 0; i < 256; i++)
-		scanner->state_tbl[state][i] = (uint8_t) scanner->accept_state;
-	return state;
-}
-
-static void transition(scanner_t *scanner, int from_state, const char *values, int to_state) {
-	for (const uint8_t *pch = (const uint8_t *) values; *pch != 0; pch++)
-		scanner->state_tbl[from_state][*pch] = (uint8_t) to_state;
-}
-
 scanner_t *scanner_new(FILE *file, const char *filename) {
 	scanner_t *scanner = calloc(1,sizeof(scanner_t));
 	if (scanner) {
@@ -77,86 +62,6 @@ scanner_t *scanner_new(FILE *file, const char *filename) {
 		scanner->line = 1;
 		scanner->col = 1;
 		scanner->text = text_new();
-
-		static const char alpha_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		static const char digit_chars[] = "0123456789";
-		static const char space_chars[] = " \r\n\t";
-		static const char misc_chars[] = "(){}[],.;:+-*/";
-
-		scanner->reject_state = new_state(scanner);
-		scanner->accept_state = new_state(scanner);
-		scanner->start_state = new_state(scanner);
-		scanner->ignore_state = new_state(scanner);
-
-		for (size_t i = 0; i < 256; i++) // default transition out of start is to reject
-			scanner->state_tbl[scanner->start_state][i] = (uint8_t) scanner->reject_state;
-
-		int drop_space_state = new_state(scanner);
-		for (int i  = 0; i < 256; i++) // reading anything other than a space takes us back to start
-			scanner->state_tbl[drop_space_state][i] = (uint8_t) scanner->ignore_state;
-		transition(scanner, drop_space_state, space_chars, drop_space_state);
-
-		int drop_line_state = new_state(scanner);
-		for (int i = 0; i < 256; i++)
-			scanner->state_tbl[drop_line_state][i] = (uint8_t) drop_line_state;
-		scanner->state_tbl[drop_line_state]['\n'] = (uint8_t) scanner->accept_state;
-
-		int scan_word_state = new_state(scanner);
-		transition(scanner, scan_word_state, alpha_chars, scan_word_state);
-		transition(scanner, scan_word_state, digit_chars, scan_word_state);
-		transition(scanner, scan_word_state, "_", scan_word_state);
-
-		int scan_integer_state = new_state(scanner);
-		transition(scanner, scan_integer_state, digit_chars, scan_integer_state);
-		int scan_frac_state = new_state(scanner);
-		transition(scanner, scan_integer_state, ".", scan_frac_state);
-		transition(scanner, scan_frac_state, digit_chars, scan_frac_state);
-
-		const char *opers[] = {
-			"||", "|=", "&&", "=="
-		};
-
-		for (int i = 0; i < _countof(opers); i++) {
-			int state = scanner->start_state;
-			for (const uint8_t *pch = (const uint8_t *) opers[i]; *pch != 0; pch++) {
-				uint8_t *tmp = &scanner->state_tbl[state][*pch];
-				int next_state = *tmp < scanner->start_state ? new_state(scanner) : *tmp;
-				scanner->state_tbl[state][*pch] = (uint8_t) next_state;
-				state = next_state;
-			}
-		}
-
-		transition(scanner, scanner->start_state, space_chars, drop_space_state);
-		transition(scanner, scanner->start_state, alpha_chars, scan_word_state);
-		transition(scanner, scanner->start_state, "_", scan_word_state);
-		transition(scanner, scanner->start_state, digit_chars, scan_integer_state);
-
-		int scan_misc_state = new_state(scanner);
-		transition(scanner, scanner->start_state, misc_chars, scan_misc_state);
-
-#if 0
-		FILE *f = NULL;
-		fopen_s(&f, "fsm_tables.h", "wb");
-
-		if (f) {
-			fprintf(f, "const uint8_t fsm_table[%d][256] = {\n", (int) scanner->num_states);
-			for (int state = 0; state < scanner->num_states; state++) {
-				fputs("\t{\n", f);
-				for (int row = 0; row < 16; row++) {
-					fputs("\t\t", f);
-					for (int col = 0; col < 16; col++) {
-						uint8_t value = scanner->state_tbl[state][16 * row + col];
-						fprintf(f, "%d, ", value);
-					}
-					fputc('\n', f);
-				}
-				fputs("\t},\n", f);
-			}
-			fputs("};\n", f);
-			fclose(f);
-		}
-#endif
-
 		read_char(scanner);
 		read_char(scanner);
 	}
@@ -175,46 +80,86 @@ static bool peek(scanner_t *scanner, int *pch) {
 	return ch != -1 ? (*pch = ch), true : false;
 }
 
-bool fsm_done(scanner_t *scanner, int state) {
-	return state == scanner->accept_state || state == scanner->reject_state || state == scanner->ignore_state;
-}
-
-uint8_t scanner_fsm(scanner_t *s) {
-	int state = s->start_state;
-	int ch;
-	while (peek(s, &ch) && !fsm_done(s, state = s->state_tbl[state][ch])) {
-		ASSERT(state != s->start_state);
-		ASSERT(state <= s->num_states);
-		read_char(s);
-		text_t *tmp = text_append(s->text, ch);
-		text_delete(s->text);
-		s->text = tmp;
-		//TRACE("fsm: ch='%c', state=%d\n", ch, state);
-	}
-	if (eos(s)) {
-		// HACK: simulate a null character at end of stream to force transition into final state
-		state = s->state_tbl[state][0];
-	}
-	return (uint8_t) state;
-}
-
-bool scanner_next(scanner_t *s, token_t *t) {
-	uint8_t end_state;
-	do {
-		text_delete(s->text);
-		s->text = text_new();
-		t->line = s->line;
-		t->col = s->col;
-		end_state = scanner_fsm(s);
-	} while (!eos(s) && end_state == s->ignore_state);
-	if (end_state == s->accept_state) {
-		t->text = s->text;
-		s->text = NULL;
+static bool take(scanner_t *scanner) {
+	if (!eos(scanner)) {
+		int ch = read_char(scanner);
+		text_t *text = text_append(scanner->text, (char) ch);
+		text_delete(scanner->text);
+		scanner->text = text;
+		return true;
 	}
 	else {
-		t->text = NULL;
-		text_delete(s->text);
-		s->text = NULL;
+		return false;
 	}
-	return end_state == s->accept_state;
+}
+
+
+static bool scan_inner(scanner_t *scanner, token_t *token) {
+	while (!eos(scanner)) {
+		if (isspace(scanner->lookahead[0])) {
+			int ch;
+			while (peek(scanner, &ch) && isspace(ch))
+				read_char(scanner);
+		}
+		else if (scanner->lookahead[0] == '/' && scanner->lookahead[1] == '/') {
+			read_char(scanner);
+			read_char(scanner);
+			int ch;
+			while (peek(scanner, &ch) && ch != '\n')
+				read_char(scanner);
+		}
+		else if (scanner->lookahead[0] == '/' && scanner->lookahead[1] == '*') {
+			read_char(scanner);
+			read_char(scanner);
+			while (!eos(scanner) && !(scanner->lookahead[0] == '*' && scanner->lookahead[1] == '/')) {
+				read_char(scanner);
+			}
+			read_char(scanner);
+			read_char(scanner);
+		}
+		else if (isdigit(scanner->lookahead[0])) {
+			int ch;
+			while (peek(scanner, &ch) && isdigit(ch))
+				take(scanner);
+			if (scanner->lookahead[0] == '.' && isdigit(scanner->lookahead[1])) {
+				take(scanner);
+				while (peek(scanner, &ch) && isdigit(ch))
+					take(scanner);
+				token->type = TT_FLOAT_LITERAL;
+			}
+			else {
+				token->type = TT_INTEGER_LITERAL;
+			}
+			return true;
+		}
+		else if (isalpha(scanner->lookahead[0]) || scanner->lookahead[0] == '_') {
+			int ch;
+			while (peek(scanner, &ch) && (isalpha(ch) || isdigit(ch) || ch == '_'))
+				take(scanner);
+			token->type = TT_IDENTIFIER;
+			return true;
+		}
+		else {
+			take(scanner);
+			token->type = 0;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool scanner_next(scanner_t *scanner, token_t *token) {	
+	while (!eos(scanner)) {
+		int line = scanner->line, col = scanner->col;
+		text_delete(scanner->text);
+		scanner->text = text_new();
+		if (scan_inner(scanner, token)) {
+			token->line = line;
+			token->col = col;
+			token->text = scanner->text;
+			scanner->text = 0;
+			return true;
+		}
+	}
+	return false;
 }
